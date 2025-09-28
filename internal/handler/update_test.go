6e6213a41c99bf629b2fa8702514aa77f9994390
Maksim <eremenko98@yandex.ru>
2025-09-28 23:12:33 +0700

    tests for server

---
+++ internal/handler/update_test.go
@@ -0,0 +1,28 @@
+// internal/handler/update_test.go
+package handler_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	handler "github.com/Maxim-hash/go-musthave-metrics-tpl/internal/handler"
+	models "github.com/Maxim-hash/go-musthave-metrics-tpl/internal/model"
+)
+
+func TestUpdateHandler_OK_Gauge(t *testing.T) {
+	st := models.NewMemStorage()
+	h := handler.UpdateHandler(st)
+
+	req := httptest.NewRequest(http.MethodPost, "/update/gauge/Alloc/123.5", nil)
+	w := httptest.NewRecorder()
+
+	h(w, req)
+	if w.Code != http.StatusOK {
+		t.Fatalf("want 200, got %d", w.Code)
+	}
+	v, ok := st.GetGauge("Alloc")
+	if !ok || v != 123.5 {
+		t.Fatalf("storage not updated: v=%v ", v)
+	}
+}
--- internal/model/memStorage.go
+++ internal/model/memStorage.go
@@ -19,3 +19,13 @@ func (ms *MemStorage) UpdateCounter(name string, value int64) {
 func (ms *MemStorage) UpdateGauge(name string, value float64) {
 	ms.gauges[name] = value
 }
+
+func (ms *MemStorage) GetCounter(name string) (int64, bool) {
+	val, ok := ms.counters[name]
+	return val, ok
+}
+
+func (ms *MemStorage) GetGauge(name string) (float64, bool) {
+	val, ok := ms.gauges[name]
+	return val, ok
+}
